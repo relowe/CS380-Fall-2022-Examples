d860f25f09078d5ba82ad73bf68ce34eeea4c266
Robert Lowe <pngwen@acm.org>
2022-09-28 13:02:08 -0500

    added windows examples

---
+++ windows/hello-win.c
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInst, 
+                     HINSTANCE hInstPrev, 
+                     PSTR cmdline, 
+                     int cmdshow) 
+{
+    return MessageBox(NULL, "Hello, world", "caption", 0);
+}
