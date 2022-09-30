4a63acc6b267c0a47317a7d81f2965c53d7ad025
Robert Lowe <pngwen@acm.org>
2022-09-30 14:23:36 -0500

    updating examples

--- windows/window.c
+++ windows/window.c
@@ -13,6 +13,7 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PSTR cmdline, int cmd
     WNDCLASSEX wc = { };
     MSG msg;
     HWND hwnd;
+    HWND hClickMe, hDontClickMe;
     int count = 0;
 
     wc.cbSize        = sizeof (wc);
@@ -40,8 +41,8 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PSTR cmdline, int cmd
                           WS_OVERLAPPEDWINDOW,
                           CW_USEDEFAULT,
                           CW_USEDEFAULT,
-                          CW_USEDEFAULT,
-                          CW_USEDEFAULT,
+                          640,
+                          480,
                           NULL,
                           NULL,
                           hInst,
@@ -51,16 +52,72 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PSTR cmdline, int cmd
         return 0;
     }
 
+    // make a huge button
+    hClickMe = CreateWindow(L"BUTTON",
+                            L"Click Me",
+                            WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_DEFPUSHBUTTON,
+                            10,   // x
+                            10,   // y
+                            290,  // width
+                            420,  // height
+                            hwnd, // parent
+                            (HMENU) 42,   // hmenu
+                            hInst, // our instance
+                            NULL); // pointer to user data
+
+    if (!hClickMe) {
+        MessageBox(NULL, TEXT("Could not create button"), NULL, MB_ICONERROR);
+        return 0;
+    }
+
+
+    hDontClickMe = CreateWindow(L"BUTTON",
+                            L"Don't Click Me",
+                            WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_DEFPUSHBUTTON,
+                            300,   // x
+                            10,   // y
+                            290,  // width
+                            420,  // height
+                            hwnd, // parent
+                            (HMENU) 43,   // hmenu
+                            hInst, // our instance
+                            NULL); // pointer to user data
+
+    if (!hDontClickMe) {
+        MessageBox(NULL, TEXT("Could not create button"), NULL, MB_ICONERROR);
+        return 0;
+    }
+
     ShowWindow(hwnd, cmdshow);
     UpdateWindow(hwnd);
     while (GetMessage(&msg, NULL, 0, 0)) {
-        printf("Message %d Received\n", ++count);
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
     return msg.wParam;
 }
+
+
 LRESULT CALLBACK winproc(HWND hwnd, UINT wm, WPARAM wp, LPARAM lp)
 {
+    if(wm == WM_DESTROY) {
+        PostQuitMessage(0);
+    }
+
+    if(wm == WM_COMMAND) {
+        // maybe a button click
+        if(HIWORD(wp) == BN_CLICKED) {
+            // it is a button click!
+            switch(LOWORD(wp)) {
+            case 42:
+                MessageBox(hwnd, TEXT("Thank you."), L"Button Message", MB_OK);
+                break;
+            case 43:
+                MessageBox(hwnd, TEXT("What's wrong with you? I told you not to click me!"), L"Button Complaint", MB_ICONERROR);
+                PostQuitMessage(-1);
+                break;
+            }
+        }
+    }
     return DefWindowProc(hwnd, wm, wp, lp);
 }
